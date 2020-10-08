a87ab4388802172b75bb251d26bd0872892f1e6a
JuanCalle1606 <jpmilojp@gmail.com>
2020-10-08 16:30:26 -0500

    Agregada la función trace

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
 	public static void main(String[] args){
-		
+		println("Por favor maximiza la consola para poder ver mejor el juego");
+	}
+	public static void trace(String txt){
+		System.out.println(txt);
 	}
 }
