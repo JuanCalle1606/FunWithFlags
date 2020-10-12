36395a4f006a5a6f0599901dc7b77a50a900edbf
JuanCalle1606 <jpmilojp@gmail.com>
2020-10-12 15:27:45 -0500

    Agregada la utilidad de limpiar la pantalla

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
 public class Main {
 	public static void main(String[] args){
-		System.out.println("Por favor maximiza la consola para poder ver mejor el juego");
+		//Ponemos la cadena que se usa para "Limpiar la pantalla"
+		Util.ClearStr = Util.repeatString(100, "\n");
+		Util.clear();
+		//mensaje inicial
+		trace(ConsoleColors.GREEN + "Por favor maximiza la consola para poder ver mejor el juego");
 	}
 	public static void trace(String txt){
 		System.out.println(txt);
--- Util.java
+++ Util.java
@@ -1,7 +1,13 @@
 public class Util {
 
+	public static String ClearStr;
+
 	public static String repeatString(int can, String con) 
 	{ 
 		return new String(new char[can]).replace("\0", con);
+	}
+
+	public static void clear(){
+		Main.trace(ClearStr);
 	}
 }
