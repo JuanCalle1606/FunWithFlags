708ad24d2be65c8f6cb2d0f0c65831191276dc43
JuanCalle1606 <jpmilojp@gmail.com>
2020-10-12 16:10:37 -0500

    Creadas las funciones principales pero aun no hacen su función

--- Main.java
+++ Main.java
@@ -5,8 +5,21 @@ public class Main {
 		Util.clear();
 		//mensaje inicial
 		trace(ConsoleColors.GREEN + "Por favor maximiza la consola para poder ver mejor el juego" + ConsoleColors.RESET);
+		trace(ConsoleColors.YELLOW+"Pulsa ENTER para iniciar"+ConsoleColors.BLACK);
+		ConsoleInput.getString();
+		trace(ConsoleColors.RESET);
+		loadFlags();
+		MainMenu();
+	}
+	public static void MainMenu(){
+		//Menu principal del juego
+		trace(Texts.getTitle()+Texts.getMainMenu());
 	}
 	public static void trace(String txt){
 		System.out.println(txt);
+	}
+	private static void loadFlags(){
+		//Esta función Cargara lel archivo de banderas y lo parceara
+		trace("Cargando banderas...");
 	}
 }
